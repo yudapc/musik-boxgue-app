9a457a00b356c258dab7f4da550d326542558d9a
Yuda Cogati <yudapc@gmail.com>
2018-04-29 21:03:32 +0700

    add gesturesEnable=false on routes and refactoring routes

--- App.js
+++ App.js
@@ -1,39 +1,8 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { StackNavigator } from 'react-navigation';
+import RootNavigator from './app/config/routes';
 import store from './app/store/store';
-import config from './app/config';
 import { registerConnectionChange } from './app/helpers/check-connection';
-import { PageLanding } from './app/components/page-landing';
-import { PageNavigation } from './app/components/page-navigation';
-import { PageHome } from './app/components/page-home';
-import { PageChords } from './app/components/page-chords';
-import { PageArtist } from './app/components/page-artist';
-import { PageHelp } from './app/components/page-help';
-import { PageChordsDetail } from './app/components/page-chords-detail';
-import { PageOffline } from './app/components/page-offline';
-
-const RootNavigator = StackNavigator(
-  {
-    PageLanding: { screen: PageLanding },
-    PageNavigation: { screen: PageNavigation },
-    PageOffline: { screen: PageOffline },
-    PageHome: { screen: PageHome },
-    PageChords: { screen: PageChords },
-    PageArtist: { screen: PageArtist },
-    PageHelp: { screen: PageHelp },
-    PageChordsDetail: { screen: PageChordsDetail }
-  },
-  {
-    initialRouteName: 'PageLanding',
-    navigationOptions: {
-      headerTintColor: config.default.color.background,
-      headerStyle: {
-        backgroundColor: config.default.color.primary
-      }
-    }
-  }
-);
 
 registerConnectionChange();
 
---
+++ app/config/routes.js
@@ -0,0 +1,35 @@
+import { StackNavigator } from 'react-navigation';
+import config from './index';
+import { PageLanding } from '../components/page-landing';
+import { PageNavigation } from '../components/page-navigation';
+import { PageHome } from '../components/page-home';
+import { PageChords } from '../components/page-chords';
+import { PageArtist } from '../components/page-artist';
+import { PageHelp } from '../components/page-help';
+import { PageChordsDetail } from '../components/page-chords-detail';
+import { PageOffline } from '../components/page-offline';
+
+export const RootNavigator = StackNavigator(
+  {
+    PageLanding: { screen: PageLanding },
+    PageNavigation: { screen: PageNavigation },
+    PageOffline: { screen: PageOffline },
+    PageHome: { screen: PageHome },
+    PageChords: { screen: PageChords },
+    PageArtist: { screen: PageArtist },
+    PageHelp: { screen: PageHelp },
+    PageChordsDetail: { screen: PageChordsDetail }
+  },
+  {
+    initialRouteName: 'PageLanding',
+    navigationOptions: {
+      headerTintColor: config.default.color.background,
+      headerStyle: {
+        backgroundColor: config.default.color.primary
+      },
+      gesturesEnabled: false
+    }
+  }
+);
+
+export default RootNavigator;
